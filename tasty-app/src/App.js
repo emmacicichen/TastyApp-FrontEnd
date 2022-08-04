a0514972bfc7562d6e7d64e94dfe604c0cf683a9
TheOldEnglishSheepdog <58194727+TheOldEnglishSheepdog@users.noreply.github.com>
2022-08-04 16:32:51 -0700

    create App.jswith antd components

--- tasty-app/src/App.js
+++ tasty-app/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
+import { Layout } from "antd";
+const { Header, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      //1.outter curly bracket: use curly braces to embed a JavaScript expression in an attribute
+      //2. inner curly bracket: obj
+      <Layout style = {{ height: "100vh"}}>
+          <Header>header</Header>
+          <Content
+              style={{
+                  padding: "50px",
+                  maxHeight: "calc(100% - 64px)",
+                  overflowY : "auto",
+              }}
+          >content</Content>
+
+      </Layout>
   );
 }
 
